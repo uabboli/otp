955f69abe5b9fdf4c5f4ade1b105dc2460892c4f
Hans Bolinder <hasse@erlang.org>
2016-09-23 12:13:54 +0200

    fixup

--- lib/typer/src/typer.erl
+++ lib/typer/src/typer.erl
@@ -119,7 +119,10 @@ extract(#analysis{macros = Macros,
 	CompOpts = dialyzer_utils:src_compiler_opts() ++ Is ++ Ds,
 	case dialyzer_utils:get_abstract_code_from_src(File, CompOpts) of
 	  {ok, AbstractCode} -> 
-	    case dialyzer_utils:get_record_and_type_info(AbstractCode) of
+            OpaqueTypes = Analysis#analysis.opaque_types,
+	    case dialyzer_utils:get_record_and_type_info(AbstractCode,
+                                                         OpaqueTypes)
+            of
 	      {ok, RecDict} ->
 		Mod = list_to_atom(filename:basename(File, ".erl")),
 		case dialyzer_utils:get_spec_info(Mod, AbstractCode, RecDict) of
